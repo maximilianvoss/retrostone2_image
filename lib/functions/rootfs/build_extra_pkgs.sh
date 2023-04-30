d254f122a08b2dbdbf4d98cc9bc312b68dad2da2
Maximilian Voss <voss@adobe.com>
2023-04-30 14:33:07 +0200

    in which our hero tries to work with the new build system

--- lib/functions/rootfs/build_extra_pkgs.sh
+++ lib/functions/rootfs/build_extra_pkgs.sh
@@ -13,7 +13,7 @@ build_extra_pkgs() {
   fetch_from_repo "$GITHUB_SOURCE/maximilianvoss/csafestring" package_name "tag:v1.8"
 
   cd "${tmp_dir}/${package_name}" || exit_with_error "can't change directory"
-  cp -R ~/rs2_image/packages/extras-buildpkgs/csafestring/* ${tmp_dir}/${package_name}
+  cp -R ~/rs2_image/packages/extras-buildpkgs/csafestring/debian ${tmp_dir}/${package_name}/DEBIAN
 
 	fakeroot_dpkg_deb_build "${tmp_dir}/${package_name}" "${DEB_STORAGE}"
 
