dd96e2c2838a051d4b74666652835771d206eee4
Maximilian Voss <voss@adobe.com>
2023-04-30 12:23:28 +0200

    in which our hero tries to work with the new build system

--- lib/functions/rootfs/build_extra_pkgs.sh
+++ lib/functions/rootfs/build_extra_pkgs.sh
@@ -1,8 +1,21 @@
 #!/usr/bin/env bash
 
-function build_extra_pkgs() {
-  env
-  set
-  gcc --version
-	return 1
+build_extra_pkgs() {
+	: "${artifact_version:?artifact_version is not set}"
+
+	delcare package_name="csafestring"
+
+	declare cleanup_id="" tmp_dir=""
+  prepare_temp_dir_in_workdir_and_schedule_cleanup package_name cleanup_id tmp_dir # namerefs
+
+  mkdir -p "${tmp_dir}/${package_name}"
+
+  fetch_from_repo "$GITHUB_SOURCE/maximilianvoss/csafestring" package_name "tag:v1.8"
+
+  cd "${tmp_dir}/${package_name}" || exit_with_error "can't change directory"
+  cp -R ~/rs2_image/packages/extras-buildpkgs/csafestring/* ${tmp_dir}/${package_name}
+
+	fakeroot_dpkg_deb_build "${tmp_dir}/${package_name}" "${DEB_STORAGE}"
+
+	done_with_temp_dir "${cleanup_id}" # changes cwd to "${SRC}" and fires the cleanup function early
 }
