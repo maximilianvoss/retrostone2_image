5f1e607dd3af18c2b6ca0a9baecf407b1ba1f1b7
Maximilian Voss <voss@adobe.com>
2023-05-02 16:50:23 +0200

    in which our hero tries to work with the new build system

--- lib/functions/rootfs/build_extra_pkgs.sh
+++ lib/functions/rootfs/build_extra_pkgs.sh
@@ -1,21 +1,32 @@
 #!/usr/bin/env bash
 
-build_extra_pkgs() {
-	: "${artifact_version:?artifact_version is not set}"
-  return 0
-#	declare package_name="csafestring"
-#
-#	declare cleanup_id="" tmp_dir=""
-#  prepare_temp_dir_in_workdir_and_schedule_cleanup package_name cleanup_id tmp_dir # namerefs
-#
-#  mkdir -p "${tmp_dir}/${package_name}"
-#
-#  fetch_from_repo "$GITHUB_SOURCE/maximilianvoss/csafestring" package_name "tag:v1.8"
-#
-#  cd "${tmp_dir}/${package_name}" || exit_with_error "can't change directory"
-#  cp -R ~/rs2_image/packages/extras-buildpkgs/csafestring/debian ${tmp_dir}/${package_name}/DEBIAN
-#
-#	fakeroot_dpkg_deb_build "${tmp_dir}/${package_name}" "${DEB_STORAGE}"
-#
-#	done_with_temp_dir "${cleanup_id}" # changes cwd to "${SRC}" and fires the cleanup function early
+function build_extra_pkgs() {
+	declare package_name="csafestring"
+
+  declare -g -r OVERLAY_WORKDIR="${WORKDIR_BASE_TMP}/overlay-workdir-${ARMBIAN_BUILD_UUID}"
+  declare -g -r OVERLAY_UPPER="${WORKDIR_BASE_TMP}/overlay-upper-${ARMBIAN_BUILD_UUID}"
+  declare -g -r OVERLAY_MERGED="${WORKDIR_BASE_TMP}/overlay-merged-${ARMBIAN_BUILD_UUID}"
+
+  mkdir -p "${OVERLAY_UPPER}"
+  mkdir -p "${OVERLAY_WORKDIR}"
+  mkdir -p "${OVERLAY_MERGED}"
+
+  mount -t overlay overlay -o lowerdir="${SDCARD}",upperdir="${OVERLAY_UPPER}",workdir="${OVERLAY_WORKDIR}" "${OVERLAY_MERGED}"
+
+  mkdir -p "${OVERLAY_MERGED}/build/${package_name}"
+
+  fetch_from_repo "$GITHUB_SOURCE/maximilianvoss/csafestring" "${package_name}" "tag:v1.8"
+
+  cd "${OVERLAY_MERGED}/build/${package_name}" || exit_with_error "can't change directory"
+  cp -R "${SRC}/cache/sources/${package_name}" "${OVERLAY_MERGED}/build/${package_name}"
+  cp -R ~/rs2_image/packages/extras-buildpkgs/csafestring/debian "${OVERLAY_MERGED}/build/debian"
+
+  dpkg-buildpackage -b -us -j4 -uc
+  find "${OVERLAY_MERGED}/build"
+  dpkg -c "${OVERLAY_MERGED}/build/*.deb"
+
+  umount "${OVERLAY_MERGED}"
+  rm -rf "${OVERLAY_UPPER}"
+  rm -rf "${OVERLAY_WORKDIR}"
+  rm -rf "${OVERLAY_MERGED}"
 }
