daf37e73ad4fc40b44a330d4b4fc3e1bdb097127
Maximilian Voss <voss@adobe.com>
2023-05-02 20:13:21 +0200

    in which our hero tries to work with the new build system

--- lib/functions/rootfs/build_extra_pkgs.sh
+++ lib/functions/rootfs/build_extra_pkgs.sh
@@ -13,12 +13,12 @@ function build_extra_pkgs() {
 
   mount -t overlay overlay -o lowerdir="${SDCARD}",upperdir="${OVERLAY_UPPER}",workdir="${OVERLAY_WORKDIR}" "${OVERLAY_MERGED}"
 
-  mkdir -p "${OVERLAY_MERGED}/build/${package_name}"
+  mkdir -p "${OVERLAY_MERGED}/build"
 
   fetch_from_repo "$GITHUB_SOURCE/maximilianvoss/csafestring" "${package_name}" "tag:v1.8"
 
   cd "${OVERLAY_MERGED}/build/${package_name}" || exit_with_error "can't change directory"
-  cp -R "${SRC}/cache/sources/${package_name}" "${OVERLAY_MERGED}/build/${package_name}"
+  cp -R "${SRC}/cache/sources/${package_name}" "${OVERLAY_MERGED}/build"
   cp -R ~/rs2_image/packages/extras-buildpkgs/csafestring/debian "${OVERLAY_MERGED}/build/${package_name}/debian"
 
   chroot "${OVERLAY_MERGED}"
